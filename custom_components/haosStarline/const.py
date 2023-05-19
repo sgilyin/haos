a3a70cb5128db76ba3ae90dfa5c40127971e5792
sgilyin <developer@ilyins.ru>
2023-05-19 16:45:48 +0500

    HACS

--- custom_components/haosStarline/const.py
+++ custom_components/haosStarline/const.py
@@ -5,7 +5,7 @@ from homeassistant.const import Platform
 
 _LOGGER = logging.getLogger(__package__)
 
-DOMAIN = "starline"
+DOMAIN = "haosstarline"
 PLATFORMS = [
     Platform.DEVICE_TRACKER,
     Platform.BINARY_SENSOR,
